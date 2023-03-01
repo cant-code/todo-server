577838eb5b8947c4c5adb9226ca59d9da8020649
cant-code <ayushc13000@gmail.com>
2023-03-01 20:22:31 +0530

    Init

---
+++ src/main/java/com/cantcode/overengineeredtodoserver/OverengineeredTodoServerApplication.java
@@ -0,0 +1,13 @@
+package com.cantcode.overengineeredtodoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverengineeredTodoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OverengineeredTodoServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cantcode/overengineeredtodoserver/OverengineeredTodoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cantcode.overengineeredtodoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverengineeredTodoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
