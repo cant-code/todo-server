b82fdda479772e9e5101afb02fd689e09ef753b5
cant-code <ayushc13000@gmail.com>
2023-06-07 17:09:12 +0530

    Fixed SecurityConfig deprecated issue

--- src/main/java/com/cantcode/overengineeredtodoserver/config/security/SecurityConfig.java
+++ src/main/java/com/cantcode/overengineeredtodoserver/config/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.cantcode.overengineeredtodoserver.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
@@ -11,12 +12,12 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain filterChain(ServerHttpSecurity httpSecurity) {
         return httpSecurity
-                .csrf().disable()
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(authorize -> authorize
                         .pathMatchers("/v3/api-docs**", "/swagger-ui**", "/webjars/swagger-ui/**", "/v3/api-docs/**", "/actuator/**")
                         .permitAll()
                         .anyExchange().authenticated())
-                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt)
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
 }
